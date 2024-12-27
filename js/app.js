c460524bd0de96f8446b9c8c7209aa5c7ab5d6dc
Bladimir Landa <bladimircarrillolanda02@gmail.com>
2024-12-27 14:22:39 -0600

    Se agregan todos los archivos

---
+++ js/app.js
@@ -0,0 +1,116 @@
+//PLUGIN
+
+$(document).ready( app() );
+
+function app() {
+
+    //Plugin: Declaración
+    $.slideShow = function(opciones) {
+
+        opcionesPlugin = $.extend({   
+            divDestino: ".slide-show",
+            slidesNum: [],
+            anchoSlide: 600,
+            intervalo: 1500,
+            colorBtn: 'yellow'
+
+        }, opciones);
+
+        const {divDestino, slidesNum, anchoSlide, intervalo, colorBtn} = opcionesPlugin;
+
+        if(slidesNum.length === 0) {
+            alert('ERROR: Los slides son necesarios');
+            return;
+        }
+
+        let actual = 0;
+        const ancho = anchoSlide;
+        const slides = slidesNum.length;
+
+        //Creación SlideShow
+        let contenido = '';
+        contenido += '<ul>';
+
+        for (let i = 0; i < slidesNum.length; i++) {
+            contenido += `<li><img src=${slidesNum[i]} alt="slide${i}"></li>`;
+        }
+
+        contenido += "</ul>";
+
+        $(divDestino).append(contenido);
+
+        const $slideShow = $(`${divDestino} ul`);
+
+        //Creación Botones
+        contenido = '';
+        contenido += '<div class="slideShowButtons">';
+
+        for (let i = 0; i < slidesNum.length; i++) {
+            contenido += `<div data-idx=${i} class="slideButton"></div>`;
+        }
+
+        contenido += '</div>';
+
+        //--after/before: Coloca el elemento como hermano
+        $(divDestino).after(contenido);
+
+        const $slideShowButtons = $('.slideShowButtons');
+
+        //Funcionalidad
+
+        $slideShowButtons.find('div').eq(0).css({
+            backgroundColor: colorBtn
+        });
+
+        const interval = window.setInterval(function() {
+            move();
+        }, intervalo);
+
+        $('.slideButton').on('click', function() {
+            let idx = $(this).data('idx');
+            idx *= -1;
+
+            const click = true;
+
+            moverxPunto(idx, click);
+        });
+
+        function move() {
+
+            actual--;
+
+            if( actual <= (slides * -1)) {
+                actual = 0;
+            }
+
+            moverxPunto(actual);
+        }
+
+        function moverxPunto(actual, click) {
+            if(click) {
+                window.clearInterval(interval);
+            }
+
+            const margen = actual * ancho;
+
+            const idx = actual * -1;
+            const $puntoActual = $slideShowButtons.find('div').eq(idx);
+            const $puntosOtros = $slideShowButtons.find('div').not($puntoActual);
+            
+
+            $slideShow.animate({
+                marginLeft: margen,
+            }, 450);
+
+            $puntoActual.css({
+                backgroundColor: colorBtn
+            });
+
+            $puntosOtros.css({
+                backgroundColor: '#a1a1a1'
+            });
+        }
+
+    }
+
+}
